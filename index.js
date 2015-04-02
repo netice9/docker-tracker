40a895ed646c498cf6bdd97d241d0bea5cfc1762
Dragan Milic <dragan@netice9.com>
2015-04-03 00:59:36 +0100

    first version of the tracker

--- index.js
+++ index.js
@@ -1,4 +1,39 @@
 'use strict';
-module.exports = function (str) {
-  console.log(str || 'Rainbow');
+
+var DockerEvents = require('docker-events');
+
+function DockerTracker(docker) {
+  var that = this;
+  this.containers = {};
+  this.docker = docker;
+
+  this.emitter = new DockerEvents({
+    docker: docker
+  });
+
+  this.emitter.on('create', function(msg) {
+    that.containers[msg.id] = docker.getContainer(msg.id);
+  });
+
+  this.emitter.on('destroy', function(msg) {
+    delete that.containers[msg.id];
+  });
+
+  docker.listContainers({all: true}, function (err, containers) {
+    if (err) {
+      throw err;
+    }
+
+    that.emitter.start();
+
+    containers.forEach(function(containerInfo) {
+      that.containers[containerInfo.Id] = docker.getContainer(containerInfo.Id);
+    });
+
+  });
+
+}
+
+module.exports = function (docker) {
+  return new DockerTracker(docker);
 };
--- test/test.js
+++ test/test.js
@@ -2,10 +2,65 @@
 'use strict';
 var assert = require('assert');
 var dockerTracker = require('../');
+var async = require('async');
+var Docker = require('dockerode');
+var docker = new Docker();
+var devNull = require('dev-null');
 
 describe('docker-tracker node module', function () {
-  it('must have at least one test', function () {
-    dockerTracker();
-    assert(false, 'I was too lazy to write any tests. Shame on me.');
+
+  // kill all running containers before each test
+
+  this.beforeEach(function(done) {
+    docker.listContainers({all: true}, function (err, containers) {
+      if (err) {
+        return done(err);
+      }
+      var killContainer = function(containerInfo, callback) {
+        var container = docker.getContainer(containerInfo.Id);
+        container.remove({force: true}, callback);
+      };
+
+      async.each(containers, killContainer, done);
+    });
   });
+
+
+  this.beforeEach(function() {
+    this.tracker = dockerTracker(docker);
+  });
+
+  describe('when there are no containers running', function(){
+    it('has not containers in the model', function() {
+      assert.deepEqual(this.tracker.containers, []);
+    });
+  });
+
+  describe('when there is one container running', function() {
+
+    this.beforeEach(function(done) {
+      docker.run('ubuntu', ['bash', '-c', 'uname -a'], devNull(), done);
+    });
+
+    it ('has one container in the model', function() {
+      assert.equal(Object.keys(this.tracker.containers).length, 1);
+    });
+
+    describe('when the container is deleted', function() {
+      this.beforeEach(function(done) {
+        var container = docker.getContainer(Object.keys(this.tracker.containers)[0]);
+        container.remove({force: true}, done);
+      });
+      this.beforeEach(function(done) {
+        setTimeout(done,400);
+      });
+      it('has not containers in the model', function() {
+        assert.deepEqual(this.tracker.containers, []);
+      });
+    });
+
+  });
+
+
+
 });
